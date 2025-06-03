0db07709148b7ba429d329da9f2b8219dbbcfb1e
islam <islomjuraev@Isloms-MacBook-Air.local>
2025-06-03 23:43:40 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+      user = JSON.parse(data);
+    }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ entended: true}));
+app.use(express.urlencoded({ entended: true }));
 
 // 2: Session code
 // 3: Views code
 app.set("views", "views");
-app.set(" view engine", "ejs");
+app.set("view engine", "ejs");
 
 // 4: Routing code
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="background: red">HELLO WORLD from ALI</h1>`);
+app.post("/create-item", (req, res) => {
 });
 
-app.get("/hello", function(req, res) {
-    res.end(`<h1>siz sovgalar sahifasidasiz</h1>`);
+app.get('/author', (req, res) => {
+    res.render("author", {user, user});
 });
 
+app.get("/", function(req, res) {
+    res.render("harid");
+});
 
 const server = http.createServer(app);
-let PORT = 3000
+let PORT = 3000;
 server.listen(PORT, function (){
     console.log(`The server is running successfully on port: ${PORT}`);
 });
