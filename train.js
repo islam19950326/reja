f5ab1eb005e546b761de724f1f0fd7fda358020e
islam <islomjuraev@Isloms-MacBook-Air.local>
2025-06-11 20:07:37 +0900

    feat: Task-B completed

--- train.js
+++ train.js
@@ -1,3 +1,38 @@
+// B-TASK
+
+function countDigits(str) {
+    return (str.match(/\d/g) || []).length;
+}
+
+const result = countDigits("ad2a54y79wet0sfgb9");
+console.log(result); 
+
+
+
+
+
+// function countDigits(str) {
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if (!isNaN(str[i]) && str[i] !== " ") {
+
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(countDigits("hello123"));
+
+
+
+
+
+
+
+
+
+
+
 // console.log("Jack Ma maslahatlari");
 // const list = [
 //     "yaxshi talaba bo'ling", // 0-20
